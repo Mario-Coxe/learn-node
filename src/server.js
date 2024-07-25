15b1bea7a890d15a07449407b345303e1e25e80a
mario.coxe <mario.coxe@ideiasdinamicas.com>
2024-07-25 16:21:17 +0100

    feat: salvando dados na memoria e retornando eles na requisição

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import http from 'node:http'
+
+
+const users = []
+
+const server = http.createServer((req, res) => {
+
+    const {method, url} = req
+
+    if(method === 'GET'){
+        console.log(users)
+        return res.end(JSON.stringify(users))
+    }
+    if(method === 'POST'){
+        users.push({
+            id: 1,
+            name: "Coxe"
+        })
+
+        return res.end("User Criado!")
+    }
+    //return res.end('Hello ')
+})
+
+server.listen(3333)
